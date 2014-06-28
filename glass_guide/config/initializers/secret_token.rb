f05d6d896bbbec499d722dd39c292bb57c7d9db3
小貫聖方 <masanori@masanori-no-MacBook-Pro.local>
2014-06-28 16:01:45 +0900

    create rails project.

---
+++ glass_guide/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlassGuide::Application.config.secret_token = 'c6182ae12be0793bf197ebda0a9f355a04d328b2c1271e40d774e04daeaa8f598c01d2c7c1a0c0cc908e84fdfc25e8c5f03c2555f2199a566a0428bfa62b2a09'
